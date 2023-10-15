4f16d4fc901577a3c4f67cee62cbd746978c0a78
aksha-prog <59757721+aksha-prog@users.noreply.github.com>
2023-10-15 11:07:32 +0530

    Add files via upload

---
+++ day1/Browser.java
@@ -0,0 +1,19 @@
+package homeassignments.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		float browserVersion = 111.0f;
+		String browserName = "Firefox";
+		boolean pageLoaded = true;
+		int noOfBrowser = 1;
+		char browserLogo = 'F';
+		
+		System.out.println(browserVersion);
+		System.out.println(browserName);
+		System.out.println(pageLoaded);
+		System.out.println(noOfBrowser);
+		System.out.println(browserLogo);
+	}
+
+}
---
+++ day1/FindNumber.java
@@ -0,0 +1,17 @@
+package homeassignments.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int a = 45;
+		if (a > 0) {
+			System.out.println("The number is positive");
+
+		} else {
+
+			System.out.println("The number is negative");
+		}
+
+	}
+
+}
